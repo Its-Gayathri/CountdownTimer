c915e0ac4fd412c2d2f3395951741d247c7984f6
gayathri u <gayathriu64@gmail.com>
2022-03-28 17:28:42 +0530

    Add project files.

---
+++ CountdownTimer/Models/ReminderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountdownTimer.Models
+{
+    public class ReminderViewModel
+    {
+        public int Id { get; set; }
+        public string ReminderName { get; set; }
+        public DateTime ReminderDate { get; set; }
+    }
+}
