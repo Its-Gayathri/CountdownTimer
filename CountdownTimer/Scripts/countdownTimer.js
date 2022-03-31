5b5995fccd0faa90fe993dc9c1ac92331ff56bb9
gayathri u <gayathriu64@gmail.com>
2022-03-31 23:55:34 +0530

    added responsive timer

--- CountdownTimer/Scripts/countdownTimer.js
+++ CountdownTimer/Scripts/countdownTimer.js
@@ -70,8 +70,13 @@ function getAllReminders(reminders) {
 };
 
 $(document).ready(function () {
-	$(".list-group-item-action").click(function () {
-		
-		//alert("The paragraph was clicked.");
+	$(".list-group-item.list-group-item-action").click(function () {
+		$('.list-group-item.list-group-item-action.active').removeClass('active');
+		$(this).addClass('active');
+		console.log(moment($(this).data("reminderdate")).format('DD/MM/YYYY'));
+		var currentDate = $(this).data("reminderdate");
+		now = new Date(),
+		startTimer = '';
+		calcTime(currentDate);
 	});
 });
