7986bc16be7d90b92d5d1ffef2942d8b6b4a2a09
gayathri u <gayathriu64@gmail.com>
2022-04-03 20:50:05 +0530

    sass update

--- CountdownTimer/ServiceProviders/Implemetations/FlowObjectForHomePage.cs
+++ CountdownTimer/ServiceProviders/Implemetations/FlowObjectForHomePage.cs
@@ -2,6 +2,7 @@
 using CountdownTimer.Entities;
 using CountdownTimer.Models;
 using CountdownTimer.ServiceProviders.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace CountdownTimer.ServiceProviders.Implemetations
@@ -16,22 +17,44 @@ namespace CountdownTimer.ServiceProviders.Implemetations
         }
         public TimerPageViewModel Flow()
         {
+            bool useDb = false;
             List<ReminderViewModel> reminderViewModel = new List<ReminderViewModel>();
-            
             TimerPageViewModel timerPageViewModel = new TimerPageViewModel();
-            List<Reminder> reminders = remindersRepo.GetAllReminders();
-            foreach (var item in reminders)
-            {
-                ReminderViewModel viewModel = new ReminderViewModel
+            if (useDb)
+            {                
+                List<Reminder> reminders = remindersRepo.GetAllReminders();
+                foreach (var item in reminders)
                 {
-                    Id = item.Id,
-                    ReminderDate = item.ReminderDate,
-                    ReminderName = item.ReminderName,
-                    ReminderType = item.ReminderType
+                    ReminderViewModel viewModel = new ReminderViewModel
+                    {
+                        Id = item.Id,
+                        ReminderDate = item.ReminderDate,
+                        ReminderName = item.ReminderName,
+                        ReminderType = item.ReminderType
+                    };
+                    reminderViewModel.Add(viewModel);
                 };
-                reminderViewModel.Add(viewModel);
-            };
-            timerPageViewModel.Reminders = reminderViewModel;
+                timerPageViewModel.Reminders = reminderViewModel;
+            }
+            else
+            {
+                List<ReminderViewModel> reminderViewModel2 = new List<ReminderViewModel>()
+                {
+                    new ReminderViewModel
+                    {
+                        Id = 1,
+                        ReminderName = "Gayathri's B'day",
+                        ReminderDate = new DateTime(2022,07,12)
+                    },
+                    new ReminderViewModel
+                    {
+                        Id = 1,
+                        ReminderName = "Divya's B'day",
+                        ReminderDate = new DateTime(2022,11,10)
+                    },
+                };
+                timerPageViewModel.Reminders = reminderViewModel2;
+            }
 
             return timerPageViewModel;
         }
